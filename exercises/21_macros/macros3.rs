be9bf7b769ae08663f013ee9cdb239bbf21e25dc
litlesnow <xuehualjx@gmail.com>
2025-12-07 23:13:36 +0800

    宏不能在文件作用域内导出，智能导出到全局

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,12 +1,12 @@
 // TODO: 在不将宏的定义语句移出这个模块的情况下修复编译器错误。
 mod macros {
+    #[macro_export]
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
 }
-
 fn main() {
     my_macro!();
 }
